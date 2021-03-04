0e0d3528134a53f13f8d6b4162858cbee1fdd1f0
jakelsaunders94 <jakelsaunders94@gmail.com>
2021-03-04 20:26:30 +0000

    Better error message if npm failed to build.

--- longclaw/core/templatetags/longclawcore_tags.py
+++ longclaw/core/templatetags/longclawcore_tags.py
@@ -9,16 +9,27 @@ VENDORS_PATH = os.path.join('core', 'js', 'vendors.bundle.js')
 
 @register.inclusion_tag("core/script.html")
 def longclaw_vendors_bundle():
-    assert os.path.exists(
-        os.path.join(os.path.dirname(os.path.dirname(__file__)), 'static', VENDORS_PATH)
-    )
+    try:
+        assert os.path.exists(
+            os.path.join(os.path.dirname(os.path.dirname(__file__)), 'static', VENDORS_PATH)
+        )
+    except AssertionError:
+        raise AssertionError("The longclaw vendors bundle does not exist in staticfiles, have you run collectstatic? "
+                             "Note: if this doesn't work, in longclaw/core/client run npm install npm run build."
+                             "Then run python manage.py collectstatic.")
     return {'path': VENDORS_PATH}
 
 @register.inclusion_tag("core/script.html")
 def longclaw_client_bundle():
-    assert os.path.exists(
-        os.path.join(os.path.dirname(os.path.dirname(__file__)), 'static', CLIENT_PATH)
-    )
+    try:
+        assert os.path.exists(
+            os.path.join(os.path.dirname(os.path.dirname(__file__)), 'static', CLIENT_PATH)
+        )
+    except AssertionError:
+        raise AssertionError("The longclaw vendors bundle does not exist in staticfiles, have you run collectstatic? "
+                             "Note: if this doesn't work, in longclaw/core/client run npm install npm run build."
+                             "Then run python manage.py collectstatic.")
+
     return {'path': CLIENT_PATH}
 
 @register.simple_tag
